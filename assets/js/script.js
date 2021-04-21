bb7799da90d438baf9dc0343c88f2c98cc20e4e5
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-21 18:55:59 +0200

    option fadein/out e slidein/out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
 $(function(){
   
   var openDropdown = $(".with_dropdown");
-  console.log(openDropdown.html());
 
+  var dropdown = $(".dropdown");
+
+  // / OPTION: HOVER ///
   // setto l'evento hover sugli elementi con classe .with_dropdown
   openDropdown.hover(function(){
     // $(this) corrisponde all'elemento che ha attivato l'evento (openDropdown)
     // $(this) cerca un elemento con classe .dropdown dentro se stesso. Se lo trova gli aggiunge la classe active
-    $(this).find('.dropdown').toggleClass("active");
+    $(this).find(dropdown).fadeToggle().toggleClass("active");
   })
 
+  /// OPTION: FADEIN E FADEOUT ///
+  // openDropdown.mouseleave(function(){
+  //   $(this).find(dropdown).fadeOut()
+  // })
+
+  // openDropdown.mouseenter(function(){
+  //   $(this).find(dropdown).fadeIn()
+  // })
+
+  /// OPTION: SLIDEDOWN E SLIDEUP ///
+  // openDropdown.click(function () {
+  //   $(this).find(dropdown).slideDown(1000);
+  // });
+  // openDropdown.dblclick(function () {
+  //   $(this).find(dropdown).slideUp(1000);
+  // });
 });
