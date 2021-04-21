ec23e58aa6e1cb6526658a574211ba22f34a685b
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-21 16:53:27 +0200

    js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(function(){
-  console.log("ciao")
-})
+  
+  var openDropdown = $("with_dropdown");
+  console.log(openDropdown);
+
+  var open = $(".open");
+  console.log(open);
+
+  open.hover(function(){
+    $('.dropdown').toggleClass("active");
+    // console.log(openDropdown)
+    // $(this).find('.dropdown').toggleClass("active");
+  })
+  
+
+});
