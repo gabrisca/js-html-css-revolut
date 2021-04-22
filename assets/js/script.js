be6218897ec62c4575ef9cf80817cd164b4a727f
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-22 09:54:43 +0200

    fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ $(function(){
 
   var dropdown = $(".dropdown");
 
-  // / OPTION: HOVER ///
+  // / OPTION 1: HOVER ///
   // setto l'evento hover sugli elementi con classe .with_dropdown
-  openDropdown.hover(function(){
-    // $(this) corrisponde all'elemento che ha attivato l'evento (openDropdown)
-    // $(this) cerca un elemento con classe .dropdown dentro se stesso. Se lo trova gli aggiunge la classe active
-    $(this).find(dropdown).fadeToggle().toggleClass("active");
-  })
+  // openDropdown.hover(function(){
+  //   // $(this) corrisponde all'elemento che ha attivato l'evento (openDropdown)
+  //   // $(this) cerca un elemento con classe .dropdown dentro se stesso. Se lo trova gli aggiunge la classe active
+  //   $(this).find(dropdown).fadeToggle().toggleClass("active");
+  // })
 
-  /// OPTION: FADEIN E FADEOUT ///
+  /// OPTION 2: FADEIN E FADEOUT ///
   // openDropdown.mouseleave(function(){
   //   $(this).find(dropdown).fadeOut()
   // })
@@ -20,11 +20,23 @@ $(function(){
   //   $(this).find(dropdown).fadeIn()
   // })
 
-  /// OPTION: SLIDEDOWN E SLIDEUP ///
+  // / OPTION 3: SLIDEDOWN E SLIDEUP ///
   // openDropdown.click(function () {
-  //   $(this).find(dropdown).slideDown(1000);
+  //   $(this).find(dropdown).toggleClass("active");
   // });
-  // openDropdown.dblclick(function () {
-  //   $(this).find(dropdown).slideUp(1000);
-  // });
+
+  // / OPTION 4: CLICK E MOUSENTER CON CHILDREN ///
+  // quando clicco attivo o rimuovo la classe active
+  openDropdown.click(function(){
+    $(this).children(dropdown).toggleClass("active");
+  })
+
+  // effetto hover al passaggio del mouse
+  openDropdown.mouseenter(function(){
+    // innanzittutto rimuovo classe active
+    dropdown.removeClass("active");
+    // con this e children vado poi ad aggiungere la classe active
+    $(this).children(dropdown).addClass("active");
+  })
+
 });
